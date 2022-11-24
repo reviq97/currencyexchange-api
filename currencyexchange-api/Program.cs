ed9a0408238c02e0066484453ea2a20e5b843acc
reviq97 <nway@o2.pl>
2022-11-24 01:35:19 +0100

    Created validators

--- currencyexchange-api/Program.cs
+++ currencyexchange-api/Program.cs
@@ -1,6 +1,10 @@
+using currencyexchange_api.Entity;
 using currencyexchange_api.Models;
 using currencyexchange_api.Services;
 using currencyexchange_api.Services.Interfaces;
+using currencyexchange_api.Validation;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Flurl.Http.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,12 +14,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddSingleton<ICurrencyRatesService, CurrencyRatesService>();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IFlurlClientFactory, PerBaseUrlFlurlClientFactory>();
 var currencyRestClientConfig = new RestClientConfig();
 builder.Configuration.GetSection("RestClients:Currency").Bind(currencyRestClientConfig);
 builder.Services.AddSingleton<IFetchContentService, FetchContentService>(param => new FetchContentService(currencyRestClientConfig));
+builder.Services.AddScoped<IValidator<ExchangeSpan>, ExchangeSpanValidator>();
 
 var app = builder.Build();
 
---
+++ currencyexchange-api/Validators/ExchangeSpanValidator.cs
@@ -0,0 +1,27 @@
+﻿using currencyexchange_api.Entity;
+using FluentValidation;
+
+namespace currencyexchange_api.Validation
+{
+    public class ExchangeSpanValidator : AbstractValidator<ExchangeSpan>
+    {
+        public ExchangeSpanValidator() 
+        {
+            RuleFor(curr => curr.currencyCodes).Must(dict =>
+            {
+                foreach (var item in dict)
+                {
+                    if (string.IsNullOrEmpty(item.Key) || string.IsNullOrEmpty(item.Value) || string.IsNullOrWhiteSpace(item.Key) || string.IsNullOrWhiteSpace(item.Value) || item.Key.Length != 3 || item.Value.Length != 3)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }).WithMessage("Currencies cannot be empty");
+
+            RuleFor(endDate => endDate.EndDate).Must(endDate => endDate.Date <= DateTime.Now || !string.IsNullOrEmpty(endDate.ToString()) || !string.IsNullOrWhiteSpace(endDate.ToString())).WithMessage("Date couldn't be higher than today or empty");
+            RuleFor(startDate => startDate.EndDate).Must(startDate => startDate.Date <= DateTime.Now || !string.IsNullOrEmpty(startDate.ToString()) || !string.IsNullOrWhiteSpace(startDate.ToString())).WithMessage("Date couldn't be higher than today or empty");
+            RuleFor(date => new { date.StartDate, date.EndDate }).Must(date => date.StartDate.Date <= date.EndDate.Date).WithMessage("Start date should be earlier than End date");
+        }
+    }
+}
